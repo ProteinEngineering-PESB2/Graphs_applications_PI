e47902c81530b9e24bc47f5b2c2d16ba77cc531a
jorgemunozsoto <jorge.munoz.s@ug.uchile.cl>
2021-10-23 07:07:36 -0300

    Data acquisition codes
    Se agregan el codigo que recolecta las ids de RCSB

---
+++ Data/Data_acquisition.py
@@ -0,0 +1,95 @@
+"""
+Script que genera un json con las ids asociadas a todas las
+categorias de proteina que cumplan con cierto parametro.
+"""
+# Importando las librerias
+import pandas as pd
+import requests
+import json
+
+
+def RCSB_Search(s, Query):
+    '''Realiza una consulta vía API a la base de datos RCSB.
+
+    Parametros:
+    s     -- Sesion de requests
+            Sesion iniciada para realizar las consultas.
+    Query -- Dict
+            Contiene los parámetros de la busqueda a realizar,
+            según lo descrito en https://search.rcsb.org/index.html
+
+    Retorna:
+            Dict
+            Contiene el resultado de la consulta realizada, según los
+            parametros indicados.
+    '''
+
+    # URL de consulta
+    URL = f'https://search.rcsb.org/rcsbsearch/v1/query?json={Query}'
+
+    # Realizando consulta
+    response = s.post(URL, json=Query,
+                      headers={'Content-Type': 'application/json'})
+
+    # Revisando la respuesta
+    if response.status_code == 200:
+        response = json.loads(response.content.decode("UTF-8"))
+
+    return response
+
+
+def Create_Dict(File, Name, Dict):
+    '''Procesa los resultados de la consulta realizada a RCSB,
+    asociando una lista de ids con su categoria correspondiente,
+    actualizando un diccionario con estos datos.
+
+    Parametros:
+    File -- Dict
+            Contiene el resultado de la consulta realizada a la
+            API de RCSB.
+    Name -- String
+            Corresponde al nombre de la categoria consultada.
+    Dict -- Dict
+            Es utilizado para guardar las ids asociadas a las
+            distintas categorias, actualizandose en cada llamada.
+
+    Retorna:
+            None
+    '''
+    # Creando una lista para guardar las ids
+    List = []
+
+    # Extrayendo las ids asociadas con una categoria
+    if type(File) is dict:
+        for protein in File['result_set']:
+            List.append(protein['identifier'])
+
+    # Actualizando el diccionario
+    Dict.update({Name: List})
+
+
+# Abriendo el archivo que contiene las categorias funcionales
+df = pd.read_csv('Categorias.csv', header=0, dtype='str')
+
+# Creando la sesion
+s = requests.session()
+
+# Creando consulta
+f = open("Search.json", "r")
+content = f.read()
+Search = json.loads(content)
+
+# Diccionario en el que se guardaran las ids
+Clasificacion = {}
+
+# Filtrando segun las distintas clasificaciones
+for index, row in df.iterrows():
+    Search['query']['nodes'][2]['parameters']['value'] = row['CATEGORIA']
+    result = RCSB_Search(s, Search)
+    Create_Dict(result, row['CATEGORIA'], Clasificacion)
+
+# Creando archivo .json para guardar la clasifiacion
+with open('Resultados.json', 'w') as fp:
+    json.dump(Clasificacion, fp)
+
+s.close()
